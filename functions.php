b7b6e62e3323c90b32f6320c646cd242eacbbb5c
yogiz <ramadhanright@gmail.com>
2017-04-05 17:15:49 +0700

    header Setting

--- functions.php
+++ functions.php
@@ -17,5 +17,6 @@ function theme_enqueue_styles() {
 	$the_theme = wp_get_theme();
 
     wp_enqueue_style( 'child-understrap-styles', get_stylesheet_directory_uri() . '/css/child-theme.min.css', array(), $the_theme->get( 'Version' ) );
+    wp_enqueue_style( 'lmnslide-styles', get_stylesheet_directory_uri() . '/style.css', array(), $the_theme->get( 'Version' ) );
     wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $the_theme->get( 'Version' ), true );
 }
